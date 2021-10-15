f0ddcff7c8cb593904f8991e692d1d8996d9610b
Kade Griffith <kadeg95@gmail.com>
2021-10-15 13:44:07 -0600

    final commit

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -8,19 +8,24 @@ const {
   validateUser,
 } = require("./auth-middleware");
 
-router.post("/register", checkUsernameFree, async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const hash = bcrypt.hashSync(password, 8);
-    const user = { username, password: hash };
-    const newUser = await Users.add(user);
-    res.status(201).json(newUser);
-  } catch (err) {
-    next(err);
+router.post(
+  "/register",
+  validateUser,
+  checkUsernameFree,
+  async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const hash = bcrypt.hashSync(password, 8);
+      const user = { username, password: hash };
+      const newUser = await Users.add(user);
+      res.status(201).json(newUser);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
-router.post("/login", checkUsernameExists, validateUser, (req, res, next) => {
+router.post("/login", validateUser, checkUsernameExists, (req, res, next) => {
   const { password } = req.body;
   if (bcrypt.compareSync(password, res.user.password)) {
     const token = buildToken(res.user);
